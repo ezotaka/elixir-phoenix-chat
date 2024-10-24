8798c00ddf07cd0628a6282585867b168f9ed0c6
Takatomo Ezo <takatomo@e20.jp>
2024-10-24 19:10:58 +0900

    Chat with WebSocket

--- lib/chat_web/endpoint.ex
+++ lib/chat_web/endpoint.ex
@@ -13,7 +13,11 @@ defmodule ChatWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
-  # Serve at "/" the static files from "priv/static" directory.
+  socket "/socket", ChatWeb.UserSocket, websocket: true, lolgpall: false
+#                                                                   socket "/socket", ChatWeb.UserSocket,
+#                                                                                             websocket: true,
+#                                                                                             longpoll: false
+#  # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
